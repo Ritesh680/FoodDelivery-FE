a1068e00bfbc34d465b30438b728d8ef0019f47c
Ritesh Poudel <ritesh@3bird.nl>
2024-12-18 22:44:04 +0545

    fix page query

--- src/component/StaticTable/index.tsx
+++ src/component/StaticTable/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import React, { useMemo } from "react";
 import { Spin, Table } from "antd";
 import type { TableColumnsType } from "antd";
 import useInnerWidth from "../../hooks/useInnerWidth";
@@ -33,6 +33,17 @@ const StaticTable = <T extends { _id: string | number; [key: string]: any }>({
 	setPage,
 }: IStaticTableProps<T>) => {
 	const { isMobileDevice } = useInnerWidth();
+	const columns = useMemo(() => {
+		const copy = [...tableHeaders];
+		copy.unshift({
+			title: "S.N.",
+			rowScope: "row",
+			render: (_item, _row, index) => {
+				return <>{((page ?? 1) - 1) * 12 + (index + 1)}</>;
+			},
+		});
+		return copy;
+	}, [tableHeaders, page]);
 	return (
 		<div className="flex flex-col w-screen">
 			{(title || actions) && (
@@ -48,16 +59,18 @@ const StaticTable = <T extends { _id: string | number; [key: string]: any }>({
 			<Table
 				bordered
 				scroll={{ x: isMobileDevice ? 1000 : 0 }}
-				columns={tableHeaders}
+				columns={columns}
 				dataSource={tableData}
 				showSorterTooltip={{ target: "sorter-icon" }}
 				className="p-5 table-auto mb-10"
 				rowKey={(record) => record._id.toString()}
 				tableLayout="auto"
 				pagination={{
+					pageSize: 12,
 					total: meta?.total,
 					current: page,
 					onChange: (page) => setPage?.(page),
+					showSizeChanger: false,
 				}}
 			/>
 		</div>
